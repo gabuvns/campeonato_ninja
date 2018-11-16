2d8445ed65f95cabbd9fb0c3348a09e816ad75e7
Gabriel <cgvilasnovas@gmail.com>
2018-11-16 21:02:49 +0000

    Implementado vetores aleatorios, bug fixes

--- main.c
+++ main.c
@@ -9,17 +9,7 @@ int main(){
   
     l_lista* lista = pega_ninja();
 
-    /*Ninja* ninja = ninja_create("Naruto", "vento", 20, 30, 40, 50);
-    insere_lista(lista, ninja);
-    ninja = ninja_create("Sasuke", "fogo", 20, 30, 40, 50);
-    insere_lista(lista, ninja);
-    ninja = ninja_create("Papaleguas", "raio", 65, 70, 49, 7);
-    insere_lista(lista, ninja);
-    
-    printf("tamanho lista = %d\n", lista->quantidade);
-    printa_lista(lista);
-    limpa_lista(lista);*/
-    //Teste segunda etapa
+  
     
     
     printa_lista(lista);
